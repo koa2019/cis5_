fdc2fd7689535460afa1b466cbf1aeb162459542
koa2019 <no-reply>
2022-04-20 23:47:34 -0700

    added floats and found win averages.
    Signed-off-by: koa2019 <no-reply>

--- project_1/project_1_battleship_v3/main.cpp
+++ project_1/project_1_battleship_v3/main.cpp
@@ -1,7 +1,7 @@
 /* 
  * File:   main.cpp
  * Author: Danielle Fernandez
- * Created on April 20, 2022,  : PM
+ * Created on April 20, 2022,  6:00 PM
  * Purpose: Project 1. Covers chapters 1-5 in Gaddis. Battleship v3
  * Version 3: 
  */
@@ -14,6 +14,7 @@
 #include <iomanip>  // fixed, setprecision()
 #include <fstream>  // ofstream 
 #include <cmath>    // round()
+
 using namespace std;
 
 // User libraries
@@ -40,6 +41,8 @@ int main(int argc, char** argv) {
     const string P1 = "Player 1 ";
     const string P2 = "Player 2 ";
 
+    bool p1_crrt,
+            p2_crrt;
     char ans;
     int nGames = 0,
             round = 1,
@@ -49,9 +52,8 @@ int main(int argc, char** argv) {
             p2Ship1,
             p1Win = 0,
             p2Win = 0;
+    float avg1, avg2, avg1Rdd;
     string gussMsg = "\nEnter a number between 1 and 14. \n";
-    bool p1_crrt,
-         p2_crrt;
 
     // open an existing file
     inputFile.open("numberOfGames.txt");
@@ -59,43 +61,53 @@ int main(int argc, char** argv) {
     // create a file to output to
     outputFile.open("Scores.txt");
 
-    // read in how many games will be played
+    // read in maximum number of games that can be played from file
     inputFile >> nGames;
 
     do {
-        // initial variables & resets variables to default values
+        // initial variables 
         p1Ship1 = rand() % (MAX - MIN) + MIN;
         p2Ship1 = rand() % (MAX - MIN) + MIN;
+
+        // reset variables to default value
         p1_crrt = p2_crrt = false;
 
-
+        // call function to display game banner
         banner("BATTLESHIP");
-
-
 
         //while ((p1Win <= nGames) && (p2Win <= nGames)) {
 
+        // loops until a player correctly guesses opponents ship location
         while ((!p1_crrt) && (!p2_crrt)) {
 
+
+            cout << endl << setw(20) << "**************" << endl;
+            cout << setw(16) << "Round " << round << endl;
+            cout << setw(20) << "**************" << endl;
+
             //*************** Player 1's Guess *************  
-            cout << endl << setw(15) << "*********" << endl;
-            cout << setw(13) << "Round " << round << endl;
-            cout << setw(15) << "*********" << endl;
-            
             cout << "\n\t" << P1 << gussMsg;
             p1Guess = rand() % (MAX - MIN) + MIN;
 
             // map the inputs to the outputs
+            // conditional checks if guess is where their opponent's ship is
             if (p1Guess == p2Ship1) {
+
+                // increment player 1 number of wins
                 p1Win++;
-                //round++;
+
+                // reassign bool value
                 p1_crrt = true;
                 //if (p1Win > 0) cout << p1Guess << " == " << p2Ship1 << endl;                
                 cout << p1Guess << " == " << p2Ship1 << endl;
                 cout << HIT << endl;
 
             } else {
-                p1_crrt = false;
+
+                // reassign bool value
+                //p1_crrt = false;
+
+                // display message for wrong guess
                 cout << p1Guess << endl;
                 cout << setw(18) << MISS;
             }
@@ -107,74 +119,100 @@ int main(int argc, char** argv) {
                 cout << "\n\t" << P2 << gussMsg;
                 p2Guess = rand() % (MAX - MIN) + MIN;
 
+                // conditional checks if guess is where their opponent's ship is
                 if (p2Guess == p1Ship1) {
-                    p2Win++;
-                    p2_crrt = true;
-                    if (p2Win > 0) cout << p2Guess << " == " << p1Ship1 << endl;
-                    cout << HIT << endl;
 
+                    // increment player 2 number of wins
+                    p2Win++;
+
+                    // reassign bool value
+                    p2_crrt = true;
+
+                    // independent if conditional displays message 
+                    // when player 2 guesses correctly
+                    if (p2Win > 0) {
+                        cout << p2Guess << " == " << p1Ship1 << endl;
+                        cout << HIT << endl;
+                    }
                 } else {
-                    //p2_crrt = false;
+
+                    // display message for wrong guess
                     cout << p2Guess << endl;
                     cout << setw(18) << MISS;
                 }
-                //if ((!p1_crrt) && (!p2_crrt)) {
-                //round++;
-                //}
             }
-            //p1_crrt = true;
-            //cout << setw(12) << "Round " << round << " over.\n";
+
+            // if both players guess wrong, then increment round by 1
+            // and display message to try guessing again
             if ((!p1_crrt) && (!p2_crrt)) {
                 round++;
                 cout << "You Both Missed. Try Again...\n";
-
             }
-            
         }
 
-        // write scores to file
-        //outputFile << P1 << "Wins: " << p1Win << endl;
-        //outputFile << P2 << "Wins: " << p2Win << endl;
-
-
-        // Display inputs to outputs
-        //banner("WINS");
+        // Display tally of player's wins
+        banner("SCOREBOARD");
         cout << setw(8) << P1 << setw(7) << left << " vs " << setw(5) << right << P2 << endl;
         cout << setw(8) << p1Win << setw(12) << p2Win << endl;
 
+        // calculate total number of won games 
         int ttlGmes = p1Win + p2Win;
 
+        // calculates each players percentage of winning
+        avg1 = p1Win / static_cast<float> (ttlGmes)*100;
+        avg2 = p2Win / static_cast<float> (ttlGmes)*100;
+
+        float num = 11.5;
+        //avg1Rdd = round(num);
+
+        // conditional checks maximum number of games has NOT been played
         if (ttlGmes < nGames) {
-            cout << "TTl Games " << ttlGmes << endl;
+            cout << "\nTotal # Games = " << ttlGmes << endl;
             cout << "Play again? ";
             cin >> ans;
 
-            //bool playAgn = false;
+            // conditional validates user's input
             if (ans == 'y') {
                 //playAgn = true;
-                round = 0;
+                round = 1;
                 cout << endl;
             } else if (ans == 'Y') {
                 //playAgn = true;
-                round = 0;
+                round = 1;
                 cout << endl;
             } else {
-                cout << "Thanks for playing!\n";
+                cout << "\nThanks for playing!\n";
+                cout << showpoint << setprecision(5);
+                cout << "\nAverages out of " << ttlGmes << " played\n";
+                cout << "Player 1 won " << avg1 << "% of the time." << endl;
+                cout << "Player 2 won " << avg2 << "% of the time." << endl;
             }
         } else {
-            float avg1 = p1Win / static_cast<float>(ttlGmes)*100;
-            float avg2 = p2Win / static_cast<float>(ttlGmes)*100;
-            //int avg1Rdd = round(avg1);
-            cout << "Averages out of " << nGames << " played\n";
+
+            // display end of game results
+            cout << showpoint << setprecision(5);
+            cout << "\nAverages out of " << nGames << " played\n";
             cout << "Player 1 won " << avg1 << "% of the time." << endl;
             cout << "Player 2 won " << avg2 << "% of the time." << endl;
-            //cout << "Player 1 won " << avg1Rdd << "% of the time." << endl;
-            cout << "Player 2 won " << avg2 << "% of the time." << endl;
+            //cout << "Player 1 won round(" << avg1 << ") = " << round(num) << "% " << endl;
+
+            //cout << "round() = " << round(num) << endl;
+            //cout << "round() = " << avg1Rdd << endl;            
+
+            // reassign ans so it will end the do..while()
             ans = 'n';
         }
+
+        // write scores to file
+        outputFile << "\nAverages out of " << nGames << " games played\n";
+        //outputFile << P1 << "Wins: " << p1Win << endl;
+        //outputFile << P2 << "Wins: " << p2Win << endl;
+        outputFile << "Player 1 won " << avg1 << "% of the time.\n"; 
+        outputFile << "Player 2 won " << avg2 << "% of the time.\n";
+
+        // continue doing all the previous statements until 
+        // ans does not equal y or Y
     } while ((ans == 'y') || ans == 'Y');
-
-
 
     // close file being read in
     inputFile.close();
