d94253c4d677eea37e049b26a4461fa3d049f429
koa2019 <no-reply>
2022-05-13 22:19:47 -0700

    Signed-off-by: koa2019 <no-reply>

--- assignment_5/assignment_5_gaddis_9thEd_Ch6_prob10_average_v1/nbproject/Package-Release.bash
+++ assignment_5/assignment_5_gaddis_9thEd_Ch6_prob10_average_v1/nbproject/Package-Release.bash
@@ -13,9 +13,9 @@ CND_BUILDDIR=build
 CND_DLIB_EXT=dll
 NBTMPDIR=${CND_BUILDDIR}/${CND_CONF}/${CND_PLATFORM}/tmp-packaging
 TMPDIRNAME=tmp-packaging
-OUTPUT_PATH=${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/assignment_5_gaddis_9thed_chap6_prob10_average_value_v1
-OUTPUT_BASENAME=assignment_5_gaddis_9thed_chap6_prob10_average_value_v1
-PACKAGE_TOP_DIR=assignment5gaddis9thedchap6prob10averagevaluev1/
+OUTPUT_PATH=${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/assignment_5_gaddis_9thed_ch6_prob10_average_v1
+OUTPUT_BASENAME=assignment_5_gaddis_9thed_ch6_prob10_average_v1
+PACKAGE_TOP_DIR=assignment5gaddis9thedch6prob10averagev1/
 
 # Functions
 function checkReturnCode
@@ -60,15 +60,15 @@ mkdir -p ${NBTMPDIR}
 
 # Copy files and create directories and links
 cd "${TOP}"
-makeDirectory "${NBTMPDIR}/assignment5gaddis9thedchap6prob10averagevaluev1/bin"
+makeDirectory "${NBTMPDIR}/assignment5gaddis9thedch6prob10averagev1/bin"
 copyFileToTmpDir "${OUTPUT_PATH}.exe" "${NBTMPDIR}/${PACKAGE_TOP_DIR}bin/${OUTPUT_BASENAME}.exe" 0755
 
 
 # Generate tar file
 cd "${TOP}"
-rm -f ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/package/assignment5gaddis9thedchap6prob10averagevaluev1.tar
+rm -f ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/package/assignment5gaddis9thedch6prob10averagev1.tar
 cd ${NBTMPDIR}
-tar -vcf ../../../../${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/package/assignment5gaddis9thedchap6prob10averagevaluev1.tar *
+tar -vcf ../../../../${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/package/assignment5gaddis9thedch6prob10averagev1.tar *
 checkReturnCode
 
 # Cleanup
--- project_1/project_1_battleship_v4/nbproject/Makefile-Release.mk
+++ project_1/project_1_battleship_v4/nbproject/Makefile-Release.mk
@@ -56,11 +56,11 @@ LDLIBSOPTIONS=
 
 # Build Targets
 .build-conf: ${BUILD_SUBPROJECTS}
-	"${MAKE}"  -f nbproject/Makefile-${CND_CONF}.mk ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/assignment_5_gaddis_9thed_ch6_prob10_average_v1.exe
+	"${MAKE}"  -f nbproject/Makefile-${CND_CONF}.mk ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/project_1_battleship_v4.exe
 
-${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/assignment_5_gaddis_9thed_ch6_prob10_average_v1.exe: ${OBJECTFILES}
+${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/project_1_battleship_v4.exe: ${OBJECTFILES}
 	${MKDIR} -p ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}
-	${LINK.cc} -o ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/assignment_5_gaddis_9thed_ch6_prob10_average_v1 ${OBJECTFILES} ${LDLIBSOPTIONS}
+	${LINK.cc} -o ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/project_1_battleship_v4 ${OBJECTFILES} ${LDLIBSOPTIONS}
 
 ${OBJECTDIR}/main.o: main.cpp
 	${MKDIR} -p ${OBJECTDIR}
